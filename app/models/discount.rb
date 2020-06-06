0570f81dd5e29cf3869124dcfee2aa44a9f41754
Jack Cullen <jpcullen20@gmail.com>
2020-06-06 16:05:37 -0600

    Add method to check if any discounts apply to an item

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -4,4 +4,8 @@ class Discount < ApplicationRecord
   validates_numericality_of :percent, greater_than: 0, less_than_or_equal_to: 100
   validates_numericality_of :item_quantity, greater_than: 0
 
+
+  def self.any_eligible?(item_quantity)
+    where("item_quantity <= ?", item_quantity).exists?
+  end
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -10,4 +10,15 @@ RSpec.describe Discount do
     it {should validate_numericality_of(:item_quantity).is_greater_than(0)}
   end
 
+  describe 'methods' do
+    it ".any_eligible" do
+      megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
+      ogre = megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
+      cart = Cart.new({ogre.id.to_s => 10})
+      megan.discounts.create(percent: 5, item_quantity: 10)
+      cart.contents[ogre.id.to_s] = 10
+      expect(megan.discounts.any_eligible?(cart.count_of(ogre.id.to_s))).to eq(true)
+    end
+  end
+
 end
