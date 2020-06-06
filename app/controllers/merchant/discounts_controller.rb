f3e6ba54ce49bc6d0f133e2112b006145d77871b
Jack Cullen <jpcullen20@gmail.com>
2020-06-06 09:16:01 -0600

    Discount id on discount index links to discount show page

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,8 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @merchant = current_user.merchant
   end
+
+  def show
+    @discount = Discount.find(params[:id])
+  end
 end
--- spec/features/discounts/index_spec.rb
+++ spec/features/discounts/index_spec.rb
@@ -13,17 +13,35 @@ RSpec.describe "Discount Index" do
       click_button 'Log In'
       expect(current_path).to eq(merchant_dashboard_path)
       click_link "View Bulk Discounts"
-      within ".discounts-#{discount1.id}" do
+      within "#discounts-#{discount1.id}" do
         expect(page).to have_content("5%")
         expect(page).to have_content("10 or more items")
       end
-      within ".discounts-#{discount2.id}" do
+      within "#discounts-#{discount2.id}" do
         expect(page).to have_content("10%")
         expect(page).to have_content("20 or more items")
       end
-      within ".discounts-#{discount3.id}" do
+      within "#discounts-#{discount3.id}" do
         expect(page).to have_content("15%")
         expect(page).to have_content("30 or more items")
       end
+    end
+
+    it "discount id links to discount show page" do
+      merchant = create(:merchant)
+      discount1 = merchant.discounts.create(percent: 5, item_quantity: 10)
+      discount2 = merchant.discounts.create(percent: 10, item_quantity: 20)
+      discount3 = merchant.discounts.create(percent: 15, item_quantity: 30)
+      user = create(:user, role: 1, merchant_id: merchant.id)
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log In'
+      expect(current_path).to eq(merchant_dashboard_path)
+      click_link "View Bulk Discounts"
+      within "#discounts-#{discount1.id}" do
+        click_link discount1.id
+      end
+      expect(current_path).to eq("/merchant/discounts/#{discount1.id}")
     end
 end
