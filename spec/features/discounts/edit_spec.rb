af6417aa1cb5bd5ac8d05198ff774512ed5baa39
Jack Cullen <jpcullen20@gmail.com>
2020-06-06 14:36:57 -0600

    Add sad path test for updating a merchant

--- spec/features/discounts/edit_spec.rb
+++ spec/features/discounts/edit_spec.rb
@@ -20,4 +20,18 @@ RSpec.describe "Discount edit" do
         expect(page).to have_content("20 or more items")
       end
   end
+  it "merchants must fill out all fields" do
+      merchant = create(:merchant)
+      discount = merchant.discounts.create(percent: 5, item_quantity: 10)
+      user = create(:user, role: 1, merchant_id: merchant.id)
+      visit login_path
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
+      click_button 'Log In'
+      visit edit_merchant_discount_path(discount.id)
+      fill_in 'Percent', with: -1
+      click_on "Update Discount"
+      expect(page).to have_content("Percent must be greater than 0")
+      expect(page).to have_button("Update Discount")
+  end
 end
