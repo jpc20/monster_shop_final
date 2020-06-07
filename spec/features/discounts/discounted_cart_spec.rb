718d23ffecf15a9bfa7f5cf2ccb084f19340e73c
Jack Cullen <jpcullen20@gmail.com>
2020-06-07 11:55:53 -0600

    Cart show now reflects the discounted subtotal and discounted grand total

--- spec/features/discounts/discounted_cart_spec.rb
+++ spec/features/discounts/discounted_cart_spec.rb
@@ -9,7 +9,9 @@ RSpec.describe "Cart discounts" do
       @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 15 )
       @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
       @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
-      @discount = @megan.discounts.create(percent: 5, item_quantity: 10)
+      @megan.discounts.create(percent: 5, item_quantity: 10)
+      @megan.discounts.create(percent: 7, item_quantity: 15)
+      @megan.discounts.create(percent: 10, item_quantity: 20)
     end
 
     it "Cart price shows merchant discounts" do
@@ -26,6 +28,7 @@ RSpec.describe "Cart discounts" do
         end
         expect(page).to have_content("Discounted Subtotal: $209.00")
       end
+      expect(page).to have_content("Total: $259.00")
     end
 
   end
