4485c2670351aea4a8158e7dcee8f98e4e945c49
Jack Cullen <jpcullen20@gmail.com>
2020-06-07 11:53:06 -0600

    Remove find eligible method from discount - it is now handled in the cart method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -2,10 +2,5 @@ class Discount < ApplicationRecord
   belongs_to :merchant
   validates_presence_of :percent, :item_quantity
   validates_numericality_of :percent, greater_than: 0, less_than_or_equal_to: 100
-  validates_numericality_of :item_quantity, greater_than: 0
-
-
-  def self.find_eligible(item_quantity)
-    where("discounts.item_quantity <= ?", item_quantity).order(percent: :desc).first
-  end
+  validates_numericality_of :item_quantity, greater_than: 0\
 end
